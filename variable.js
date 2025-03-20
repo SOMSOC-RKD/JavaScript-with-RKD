d1806311f8fc65342250da94cf6999b4db7fb11e
SOMSOC-RKD <das794176@gmail.com>
2025-03-20 22:35:03 +0530

    add variable class

---
+++ variable.js
@@ -0,0 +1,20 @@
+const accountId = 144533
+let accountEmail = "das4741@gmail.com"
+var accountPassword = "12345"
+accountCity = "kolkata"
+
+// accountId = 2 not alowed
+
+
+accountEmail = "dasr4545@gmail.com"
+accountPassword = "54321"
+accountCity = "bihar"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+not use var, use let , const
+bcoz of issue in block scope and functional scope
+*/
