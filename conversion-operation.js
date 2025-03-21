88d05fe1eda3f729362e7ab48fae67b1a5fc7565
SOMSOC-RKD <das794176@gmail.com>
2025-03-21 22:20:31 +0530

    conversion of datatype

---
+++ conversion-operation.js
@@ -0,0 +1,29 @@
+let score = "99ab"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "99" => 99 easily convert string 2 number
+// "99ab" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log( booleanIsLoggedIn);
+ 
+//  1 => true , 0 => false
+// "" => f
+// "rkd" => t
+
+
+
+// *****************number to string convert*****************
+let someNumber = 99
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
