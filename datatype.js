07dbe2df93d07263f955d8486895f34ddab102b5
SOMSOC-RKD <das794176@gmail.com>
2025-03-21 21:44:29 +0530

    disccus primitive datatypes

--- datatype.js
+++ datatype.js
@@ -1,8 +1,26 @@
- "use strict" // treat all js code as new version
+ "use strict"; // treat all js code as new version
 
 
 
-alert(3+3)
+// alert(3+3) we are using node.js not browser so here alert not showing
 
+let name = "rkd"
+let age = 28
+let isloggedIn = false
+
+// **********premitive data type****************//
+
+
+// number => 2 to the power 52
+// bigint => if number is too large
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// console.log(typeof variableName);  sample
+
+console.log(typeof undefined);
 
 
