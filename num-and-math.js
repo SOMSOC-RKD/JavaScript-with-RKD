bd2487cb32b2c2af202076a97ebbada27e9bc45a
SOMSOC-RKD <das794176@gmail.com>
2025-06-02 22:49:46 +0530

    done

--- num-and-math.js
+++ num-and-math.js
@@ -3,3 +3,18 @@ console.log(score);
 
 const balance = new Number(800);
 console.log(balance);
+
+console.log(balance.toString().length);
+// 800.00 
+console.log(balance.toFixed(2));
+// 800.0
+console.log(balance.toFixed(1));
+
+
+// automatic round figure converted with two digit no 
+const otherNumber = 25.956
+console.log(otherNumber.toPrecision(2));
+console.log(otherNumber.toPrecision(3));
+
+const hundred = 1000000000
+console.log(hundred.toLocaleString());
