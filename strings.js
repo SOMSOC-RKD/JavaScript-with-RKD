054802e5e8f94e3d5675c5fbd1739eb99c659dec
SOMSOC-RKD <das794176@gmail.com>
2025-06-02 10:49:59 +0530

    done

--- strings.js
+++ strings.js
@@ -7,4 +7,22 @@ const age = 25
 
 
 //  in new method [``] is used 
-console.log(`Hellow my name is ${name}.I am ${age} year old.`)
+// console.log(`Hellow my name is ${name}.I am ${age} year old.`)
+
+// here [0=U,1=b, 2= i, 3=s, 4=o , 5=f 6=t]
+const gameName = new String('Ubisoft')
+console.log(gameName[0]);
+console.log(gameName[1]);
+console.log(gameName[2]);
+console.log(gameName[3]);
+console.log(gameName[4]);
+console.log(gameName[5]);
+console.log(gameName[6]);
+
+console.log(gameName.length);
+console.log(gameName.__proto__);
+console.log(gameName.toUpperCase());
+// position of character[charAt]
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('U'));
+
