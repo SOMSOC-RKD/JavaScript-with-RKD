cc40d9588f4608a0a2b145a52cd052af674065b5
SOMSOC-RKD <das794176@gmail.com>
2025-06-02 11:13:35 +0530

    done

--- strings.js
+++ strings.js
@@ -26,3 +26,24 @@ console.log(gameName.toUpperCase());
 console.log(gameName.charAt(3));
 console.log(gameName.indexOf('U'));
 
+// here  0 to 5 but 5 not counted only 0,1,2,3,4 is counted,,, in "substrings" nagative value not showing
+const newString = gameName.substring(0,5)
+ console.log(newString);
+
+//   in "slice" nagative value is showing
+
+const anotherString = gameName.slice (-8,5)
+ console.log(anotherString);
+
+//  trim/ clean space
+
+const newStringOne = "     Ram    Das    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://github.com/SOMSOC-RKD/JavaScript-with-RKD/blob/main/strings.js"
+console.log(url.replace ('strings', 'string'));
+
+// to identify letter or object in perticular URL is present or Not
+console.log(url.includes('RYT'));
+
