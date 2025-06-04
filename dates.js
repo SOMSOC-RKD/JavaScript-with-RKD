0a3265d9b007255ddc71669f7fd8e3272a7c11d3
SOMSOC-RKD <das794176@gmail.com>
2025-06-04 12:16:56 +0530

    new date

---
+++ dates.js
@@ -0,0 +1,31 @@
+// Dates,,,,,,,,,,,,,,,
+
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate = new Date(2023,1,15);
+// console.log(myCreatedDate.toLocaleString());
+
+let myCreatedDate = new Date("2025-08-05");
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// here all resulted value in [mili second/ms] 
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+// console.log(Math.round(Date.now()/1000000));
+
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate);
