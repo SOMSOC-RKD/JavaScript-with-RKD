960955e4caccda27d55004a2d8a1d34dbcfdb10f
SOMSOC-RKD <das794176@gmail.com>
2025-06-04 11:09:59 +0530

    d

--- num-and-math.js
+++ num-and-math.js
@@ -1,26 +1,27 @@
-const score = 500
-console.log(score);
+// const score = 500
+// console.log(score);
 
-const balance = new Number(800);
-console.log(balance);
+// const balance = new Number(800);
+// console.log(balance);
 
-console.log(balance.toString().length);
+// console.log(balance.toString().length);
 // 800.00 
-console.log(balance.toFixed(2));
+// console.log(balance.toFixed(2));
 // 800.0
-console.log(balance.toFixed(1));
+// console.log(balance.toFixed(1));
 
 
 // automatic round figure converted with two digit no 
-const otherNumber = 25.956
-console.log(otherNumber.toPrecision(2));
-console.log(otherNumber.toPrecision(3));
+// const otherNumber = 25.956
+// console.log(otherNumber.toPrecision(2));
+// console.log(otherNumber.toPrecision(3));
 
 const hundred = 1000000000
-console.log(hundred.toLocaleString('en-IN'));
-console.log(hundred.toLocaleString());
+// console.log(hundred.toLocaleString('en-IN'));
+// console.log(hundred.toLocaleString());
 
 // ************************* MATH *************************
 
 console.log(Math);
-console.log(math.abs());
+console.log(math.abs(-4));
+console.log(math.round(4));
