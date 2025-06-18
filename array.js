7b955a98294d2533d71ea85d675caf51fc4f21c4
SOMSOC-RKD <das794176@gmail.com>
2025-06-18 12:30:51 +0530

    done

---
+++ array.js
@@ -0,0 +1,10 @@
+// arrays  ,,,,, array are write all time in "[]" = brackets, not Braces = "{}"
+// data put within array is called "Elements"
+// const myArr = [1,2,3,4,5,6, "RKD",25]
+// console.log(myArr[0]);
+
+const myArr = [0,1,2,3,4,5,6]
+const myHeros = ["hanuman", "mahadev"]
+
+const myArr2 = new Array(1,2,3,4,5)
+ console.log(myArr[1]);
